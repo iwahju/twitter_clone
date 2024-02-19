7ead31e5038d5ece64ea09f6a704ce43158de7db
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-02-19 12:51:43 -0600

    created user scheme

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 
+const connect = () => {
+    mongoose.set("strictQuery", false);
+    mongoose
+      .connect(process.env.MONGO)
+      .then(() => {
+        console.log("connect to mongodb database");
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+app.get("/",(req, res)=>{
+    res.send("Hello!");
+})
 
 app.listen(8000,() => {
+    connect();
     console.log("Listening to port 8000");
 });
 
