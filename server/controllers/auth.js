a570bb31612176ad597bc18fd385570bd0708c1a
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-02-28 22:14:21 -0600

    Sign In Function added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { handlerError } from "../error.js";
 
 export const signup = async(req,res, next)=>{
     try {
@@ -11,10 +12,34 @@ export const signup = async(req,res, next)=>{
         await newUser.save();
 
         const token = jwt.sign({id: newUser._id}, process.env.JWT);
+        
+        const {password,...othersData} = newUser._doc;
         res.cookie('access_token', token,{
             httpOnly: true,
+        }).status(200).json(othersData);
+    }catch(err){
+        next(err);
+    }
+};
 
-        }).status(200).json(newUser);
+
+
+
+export const signin = async(req,res, next)=>{
+    try {
+        const user = await User.findOne({username:req.body.username});
+
+        if(!user) return next(handlerError(404, "User not found"));
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+        
+        if(!isCorrect) return next(handlerError(404,"Wrong password"));
+
+        const token = jwt.sign({id: user._id}, process.env.JWT);
+        const {password, ...othersData} = user._doc;
+
+        res.cookie( 
+            "access_token", token, {httpOnly:true}
+        ).status(200).json(othersData);
     }catch(err){
         next(err);
     }
