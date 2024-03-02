9cfe63e5eadc4907c237b3d0df07592b94e357f3
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-03-01 21:32:14 -0600

    Added Following and Delete User function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -29,3 +29,44 @@ export const update = async (req, res, next) => {
     return next(handlerError(403, "You can only update your own account"));
   }
 };
+
+
+export const deleteUser = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+  
+        res.status(200).json("User delete");
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return next(handlerError(403, "You can only update your own account"));
+    }
+  };
+
+
+  export const follow = async (req, res, next) => {
+      try {
+        // user
+        const  user = await User.findById(req.params.id);
+        // currect user
+        const currentUser = await User.findById(req.body.id);
+
+        await User.findByIdAndDelete(req.params.id);
+
+        if(!user.followers.includes(req.body.id)){
+            await user.updateOne({
+                $push: { followers:req.body.id},
+            });
+            await currentUser.updateOne({
+                $push:{following:req.params.id}
+            });
+        } else {
+            res.status(403).json('You already follow this user');
+        }
+        res.status(200).json('following this user');
+      } catch (err) {
+        next(err);
+      }
+  };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { getUser, update } from "../controllers/user.js";
+import { getUser, update, deleteUser,follow } from "../controllers/user.js";
 import { verifyToken } from "../verifyToken.js";
 
 
 const router = express.Router();
 
+//update user
 router.put("/:id",verifyToken, update);
 
+
+// get user 
 router.get("/find/:id", getUser);
 
+
+// delete user
+router.delete("/:id", verifyToken,deleteUser);
+
+// follow
+router.put("/follow/:id",verifyToken,follow)
 export default router;
