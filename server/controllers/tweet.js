4fe372669ad6fb7cc336ffb652a2181f0a80c1ab
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-03-04 18:46:05 -0600

    tweet.js update

--- server/controllers/tweet.js
+++ server/controllers/tweet.js
@@ -12,19 +12,17 @@ export const createTweet = async (req, res, next) => {
   }
 };
 
-export const deleteTweet = async (req,res,next) => {
-
+export const deleteTweet = async (req, res, next) => {
   try {
     const tweet = await Tweet.findById(req.params.id);
-    if (tweet.userId === req.body.id){
+    if (tweet.userId === req.body.id) {
       await tweet.deleteOne();
       res.status(200).json("tweet has been deleted");
     } else {
-      handleError(500,err);
+      handleError(500, err);
     }
-
   } catch (err) {
-    handleError(500,err);
+    handleError(500, err);
   }
 };
 
