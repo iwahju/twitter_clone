bf5e5b5db405bb1b75aa18e289ba07d1c0e97fdf
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-03-06 11:06:15 -0600

    Leftside Bar updated

--- client/src/components/LeftSideBar/LeftSideBar.jsx
+++ client/src/components/LeftSideBar/LeftSideBar.jsx
@@ -5,14 +5,16 @@ import HomeIcon from '@mui/icons-material/Home';
 import TagIcon from '@mui/icons-material/Tag';
 import PersonIcon from '@mui/icons-material/Person';
 import { Link } from 'react-router-dom';
+import Home from '@mui/icons-material/Home';
 
 const LeftSideBar = () => {
   return (
     <div className='flex flex-col h-full md:h-[90vh] justify-between mr-6'>
         <div className='mt-6 flex flex-col space-y-4 '>
-            <Link tp="/">
-                <div>
-                    
+            <Link to="/">
+                <div className='flex items-center space-x-6 px-2 py-2 hover:bg-slate-200 rounded-full cursor-pointer'>
+                    <HomeIcon fontSize='large'/>
+                    <p>Home</p>
                 </div>
             </Link>
         </div>
