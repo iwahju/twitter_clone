1e39e1958c698601be37cd4e415d85b49912ebc5
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-03-06 23:10:02 -0600

    Created Proxy

--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export const Signin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/signin" , { username, password });
+      console.log('res',res.data)
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <form className="bg-gray-200 flex flex-col py-12 px-8 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
       <h2 className="text=3xl font-bold text-center">Sign in to Twitter</h2>
 
       <input
+        onChange={(e) => setUsername(e.target.value)}
         type="text"
         placeholder="username"
         className="text-xl py-2 rounded-full px-4"
       />
       <input
+        onChange={(e) => setPassword(e.target.value)}
         type="password"
         placeholder="password"
         className="text-xl py-2 rounded-full px-4"
       />
 
-      <button className="text-xl py-2 rounded-full px-4 bg-blue-500 text-white">
+      <button
+        className="text-xl py-2 rounded-full px-4 bg-blue-500 text-white"
+        onClick={handleLogin}
+      >
         Sign in
       </button>
       <p className="text-center text-xl">Don't have an account?</p>
