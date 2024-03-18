2fdd1bbd2cbd4f9a306f7dc8bdb0ab818a9634fa
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-03-17 22:41:45 -0500

    Add Redux-Persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,38 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistedReducer(persistConfig, rootReducer);
+
 const rootReducer = combineReducers({ user: userReducer });
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+
+export const persistor = persistStore(store);
